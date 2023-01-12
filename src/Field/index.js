1d62bd19a573c8d8236fc3c1523052740b286ebf
guangting <guangting.wang@kroger.com>
2023-01-12 08:57:37 -0500

    WIP

--- src/Field/index.js
+++ src/Field/index.js
@@ -8,7 +8,7 @@ export default function Field({data, fieldName}) {
     <div className='pr-8'>
       <p className='pt-8 pb-8 bold ls-4'>{val}</p>
       <hr style={{ border: '1px solid rgba(232, 77, 77, 0.198)' }}/>
-      <p className='pt-4 pb-8' style={{ fontSize: '12px', color: 'rgba(232, 77, 77, 0.298)' }}>{fieldName.toUpperCase()}</p>
+      <p className='pt-4 pb-8' style={{ fontSize: '12px', color: 'rgba(232, 77, 77, 0.390)' }}>{fieldName.toUpperCase()}</p>
     </div>
   )
 }
